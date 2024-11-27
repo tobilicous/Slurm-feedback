7b738fa663fcf36f988136764babbca46f6f08d8
Tobi Adesanya <tobiadesanya103@gmail.com>
2024-11-27 13:35:41 -0500

    Fix Signup.css path issue

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import 'src/components/Signup.css';
+import '../styles/Signup.css';
 
 const Signup = () => {
   const [formData, setFormData] = useState({ name: "", email: "" });
